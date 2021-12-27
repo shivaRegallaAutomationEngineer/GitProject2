ae3d7a0d1339c4aaa87e3b27de70fc8c6164171b
shivaRegallaAutomationEngineer <shivarrac@gmail.com>
2021-12-27 22:35:49 +0530

    fourth commit

--- src/JavaPrograms/FirstProgram.java
+++ src/JavaPrograms/FirstProgram.java
@@ -5,7 +5,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hi java how are you");
 		System.out.println("Hi java how are you");
-		System.out.println("Hi java how are you");
+		
 	}
 
 }
